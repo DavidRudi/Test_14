1a668c24a67585bcd849c3d7e5ba677732cba556
muhammadalianda <muhammadalianda@yahoo.com>
2021-04-26 12:30:48 +0700

    [UPD] Data Test

--- fal_connect_task_meeting/tests/test_project_task.py
+++ fal_connect_task_meeting/tests/test_project_task.py
@@ -10,6 +10,13 @@ class TestProjectTask(TestProjectCommon):
     def setUpClass(cls):
         super(TestProjectTask, cls).setUpClass()
 
+        Users = cls.env['res.users'].with_context({'no_reset_password': True})
+        cls.user_projecttask = Users.create({
+            'name': 'Didier Drogba',
+            'login': 'D',
+            'email': 'drogba@example.com',
+            'groups_id': [(6, 0, [user_group_employee.id, user_group_project_manager.id])]})
+
         cls.project_test = cls.env['project.project'].with_context({'mail_create_nolog': True}).create({
             'name': 'Connect To Meeting',
             'privacy_visibility': 'portal',
@@ -20,4 +27,4 @@ class TestProjectTask(TestProjectCommon):
             'user_id': cls.self.env.user,
             'planned_date_begin': "2021-04-07 11:00:00",
             'planned_date_end': "2021-04-07 15:00:00",
-            'project_id': cls.project_test.id})
+            'project_id': cls.user_projecttask.id})
